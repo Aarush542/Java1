76b49b15e112de65805ebee9bb2f66bf8b72da04
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-09-02 15:12:45 +0530

    Second Commit

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,13 @@
-i
+// 1) Write a Java program to find the sum of all elements in an integer array.
+/**
+ * A
+ */
+public class A {
+
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
