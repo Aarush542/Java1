4e0c2e15ecca5c8cbe1562dc9cdf5b00bd5e4324
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-09-18 16:47:37 +0530

    Commit

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,18 +1,1 @@
-import java.util.*;
-public class pallindromearray {
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter size");
-        int size=sc.nextInt();  
-        int a[]=new int[size];
-        for(int i=0;i<a.length;i++){
-            System.out.println("Enter array");
-            a[i]=sc.nextInt();
-        }
-
-        for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
-        }
-
-    }
-}
+Pallindromenumber
